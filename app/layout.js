16047022f189b67abdbe237173d52dbcce764538
thekavikumar <kavikumarceo@gmail.com>
2024-02-12 20:01:59 +0530

    added speed insights

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 import Launch from "@/components/Launch";
+import ParticleWrapper from "@/components/ParticleWrapper";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Tantrotsav 2024",
@@ -13,14 +15,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black flex flex-col text-white">
-        {/* <Navbar />
-        <SocialMedia />
-        <Toaster /> */}
-        {children}
-        {/* <div className="flex-1">{children}</div>
+        <ParticleWrapper>
+          <Navbar />
+          <SocialMedia />
+          <Toaster />
+          {children}
+          {/* <div className="flex-1">{children}</div>
         {/* <div className="flex-1">
           <Launch />
         </div> */}
+        </ParticleWrapper>
+        <SpeedInsights />
       </body>
     </html>
   );
---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="h-screen flex items-center justify-center w-screen">
+      <h1>Loading</h1>
+    </div>
+  );
+}
+
+export default loading;
