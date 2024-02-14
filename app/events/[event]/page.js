d9181a71a6c450bf1166b80f38466851627041ea
thekavikumar <kavikumarceo@gmail.com>
2024-02-14 15:11:42 +0530

    resolved cart

--- app/events/[event]/page.js
+++ app/events/[event]/page.js
@@ -7,68 +7,67 @@ import Image from "next/image";
 /*<p>Name: {item.eventTitle}</p>
 			<p>ID: {item._id}</p> */
 const ListItem = ({ item }) => {
-	console.log(item.eventImage.image);
-	return (
-		<div className="">
-			<div className="pt-[7%] pl-[7%] flex flex-col md:flex-row gap-10">
-				<div className=" pt-14 md:pt-2">
-					<Image
-						src={item.eventImage.image}
-						height={500}
-						width={500}
-						alt="eventimage"
-					/>
-					<div className="flex flex-row justify-between">
-						<div>Club</div>
-						<div className="font-semibold hover:border-l-4 transition-all hover:font-bold outline-1 p-2 hover:bg-white hover:text-black ">
-							Add to cart
-						</div>
-					</div>
-				</div>
-				<div className="flex flex-col gap-5 col-span-1 sm:row-span-1 pb-24 md: p-2">
-					<div>
-						<h1 className="text-3xl">{item.eventTitle}</h1>
-					</div>
-					<div>
-						<p>{item.eventDescription}</p>
-					</div>
-					<div className="flex flex-row gap-1">
-						<p>Registration Fee : {item.registrationFee}</p>
-					</div>
-					<div>
-						<p>Venue : {item.venue}</p>
-					</div>
-					<div className="flex flex-row gap-10">
-						<div className="flex flex-col">
-							<p>First Price : </p>
-							<p>Second Price : </p>
-							<p>Third Price : </p>
-						</div>
-						<div className="flex flex-col">
-							<p>{item.firstPrice}</p>
-							<p>{item.secondPrice}</p>
-							<p>{item.thirdPrice}</p>
-						</div>
-					</div>
-					<div className="flex flex-row gap-2 ">
-						<p>Hosted by : {item.club}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="">
+      <div className="pt-[7%] pl-[7%] flex flex-col md:flex-row gap-10">
+        <div className=" pt-14 md:pt-2">
+          <Image
+            src={item.eventImage.image}
+            height={500}
+            width={500}
+            alt="eventimage"
+          />
+          <div className="flex flex-row justify-between">
+            <div>Club</div>
+            <div className="font-semibold hover:border-l-4 transition-all hover:font-bold outline-1 p-2 hover:bg-white hover:text-black ">
+              Add to cart
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 col-span-1 sm:row-span-1 pb-24 md: p-2">
+          <div>
+            <h1 className="text-3xl">{item.eventTitle}</h1>
+          </div>
+          <div>
+            <p>{item.eventDescription}</p>
+          </div>
+          <div className="flex flex-row gap-1">
+            <p>Registration Fee : {item.registrationFee}</p>
+          </div>
+          <div>
+            <p>Venue : {item.venue}</p>
+          </div>
+          <div className="flex flex-row gap-10">
+            <div className="flex flex-col">
+              <p>First Price : </p>
+              <p>Second Price : </p>
+              <p>Third Price : </p>
+            </div>
+            <div className="flex flex-col">
+              <p>{item.firstPrice}</p>
+              <p>{item.secondPrice}</p>
+              <p>{item.thirdPrice}</p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-2 ">
+            <p>Hosted by : {item.club}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 async function Page({ params }) {
-	const event = await getEventById(params.event);
-	return (
-		<div className="">
-			{event.map((item, index) => (
-				// Step 3: Render the array of JSX elements
-				<ListItem key={index} item={item} />
-			))}
-		</div>
-	);
+  const event = await getEventById(params.event);
+  return (
+    <div className="">
+      {event.map((item, index) => (
+        // Step 3: Render the array of JSX elements
+        <ListItem key={index} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default Page;
--- components/CheckoutCard.js
+++ components/CheckoutCard.js
@@ -1,80 +1,80 @@
 "use client";
-import { useUserDetails } from "@/context/zustand";
+import { useCartDetails, useUserDetails } from "@/context/zustand";
 import { db } from "@/firebase";
 import { get, ref, set, update } from "firebase/database";
 import toast from "react-hot-toast";
 
 const CheckoutCard = ({ event }) => {
-	const { user } = useUserDetails();
-	// const { cart, setCart } = useCartDetails();
-	const userId = user?.uid;
-	const userCartRef = ref(db, "users/" + userId + "/cart/");
-	const updateCartRef = ref(db, "users/" + userId);
-	const itemId = event.id;
+  const { user } = useUserDetails();
+  const { setCart } = useCartDetails();
+  const userId = user?.uid;
+  const userCartRef = ref(db, "users/" + userId + "/cart/");
+  const updateCartRef = ref(db, "users/" + userId);
+  const itemId = event.id;
 
-	async function handleDelete() {
-		try {
-			await get(userCartRef).then((snapshot) => {
-				if (snapshot.exists()) {
-					const cart = snapshot.val();
-					const itemIndex = cart.findIndex((item) => item.id === itemId);
+  async function handleDelete() {
+    try {
+      await get(userCartRef).then((snapshot) => {
+        if (snapshot.exists()) {
+          const cart = snapshot.val();
+          const itemIndex = cart.findIndex((item) => item.id === itemId);
 
-					if (itemIndex !== -1) {
-						const updatedCart = cart.filter(
-							(item, index) => index !== itemIndex
-						);
+          if (itemIndex !== -1) {
+            const updatedCart = cart.filter(
+              (item, index) => index !== itemIndex
+            );
 
-						try {
-							// Use the update function to update only the 'cart' property
-							update(updateCartRef, { cart: updatedCart });
-							// setCart(updatedCart);
-							toast.success("Item removed from Firebase!");
-						} catch (error) {
-							console.error("Error updating Firebase:", error);
-							toast.error("Error updating Firebase: " + error.message);
-						}
-					} else {
-						toast.error("Item not found in the cart");
-					}
-				} else {
-					console.log("No data available");
-				}
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	}
+            try {
+              // Use the update function to update only the 'cart' property
+              update(updateCartRef, { cart: updatedCart });
+              setCart(updatedCart);
+              toast.success("Item removed from Firebase!");
+            } catch (error) {
+              console.error("Error updating Firebase:", error);
+              toast.error("Error updating Firebase: " + error.message);
+            }
+          } else {
+            toast.error("Item not found in the cart");
+          }
+        } else {
+          console.log("No data available");
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	return (
-		<div className="nft flex items-center cursor-pointer	">
-			<div className="main w-full flex items-center gap-3">
-				<img className="tokenImage" src={event.eventImage.image} alt="image" />
-				<div className="flex items-center w-full justify-between">
-					<h2 className="title text-lg font-semibold">{event.eventTitle}</h2>
-					<h2 className="title text-lg font-semibold">
-						₹ {event.registrationFee}
-					</h2>
-				</div>
-				<div
-					className="tokenInfo w-full cursor-pointer hover:text-black"
-					onClick={handleDelete}
-				>
-					<button className="price text-[#ee83e5] hover:text-black duration-200 ease-in-out w-full text-center rounded-md hover:bg-[#ee83e5] ">
-						Remove from Cart
-					</button>
-				</div>
-				<hr />
-				<div className="creator">
-					<div className="wrapper">
-						<img src={event.clubImage.image} />
-					</div>
-					<p>
-						<ins>Creation of</ins> {event.club}
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="nft flex items-center cursor-pointer	">
+      <div className="main w-full flex items-center gap-3">
+        <img className="tokenImage" src={event.eventImage.image} alt="image" />
+        <div className="flex items-center w-full justify-between">
+          <h2 className="title text-lg font-semibold">{event.eventTitle}</h2>
+          <h2 className="title text-lg font-semibold">
+            ₹ {event.registrationFee}
+          </h2>
+        </div>
+        <div
+          className="tokenInfo w-full cursor-pointer hover:text-black"
+          onClick={handleDelete}
+        >
+          <button className="price text-[#ee83e5] hover:text-black duration-200 ease-in-out w-full text-center rounded-md hover:bg-[#ee83e5] ">
+            Remove from Cart
+          </button>
+        </div>
+        <hr />
+        <div className="creator">
+          <div className="wrapper">
+            <img src={event.clubImage.image} />
+          </div>
+          <p>
+            <ins>Creation of</ins> {event.club}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutCard;
