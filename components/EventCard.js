d05e0b8d50cdb1e663897a4a19cf8315fd4b64cf
thekavikumar <kavikumarceo@gmail.com>
2024-02-16 10:58:56 +0530

    done

--- components/EventCard.js
+++ components/EventCard.js
@@ -60,6 +60,8 @@ const EventCard = ({ event }) => {
       });
   };
 
+  console.log(urlForImage(event.eventImage));
+
   return (
     <div className="nft z-30 flex items-center cursor-pointer	">
       <div className="main w-full flex items-center gap-3">
