5bae82fb81465ccf5d1c3487e357c5a21abb9c5b
thekavikumar <kavikumarceo@gmail.com>
2024-02-21 12:42:15 +0530

    added so

--- app/cart/page.js
+++ app/cart/page.js
@@ -10,189 +10,189 @@ import ShortUniqueId from "short-unique-id";
 import toast from "react-hot-toast";
 
 function Page() {
-	const { cart, setCart, removeAll } = useCartDetails();
-	const idGen = new ShortUniqueId({ length: 10 });
-	const orderId = idGen.rnd();
-	const [totalAmount, setTotalAmount] = useState(0);
-	const { user } = useUserDetails();
-	const userId = user?.uid;
-	const userCartRef = ref(db, "users/" + userId + "/cart/cart/");
-	useEffect(() => {
-		// Fetch user cart data when the userCartRef changes
-		const unsubscribe = onValue(userCartRef, (snapshot) => {
-			if (snapshot.exists()) {
-				const newCart = snapshot.val();
+  const { cart, setCart, removeAll } = useCartDetails();
+  const idGen = new ShortUniqueId({ length: 10 });
+  const orderId = idGen.rnd();
+  const [totalAmount, setTotalAmount] = useState(0);
+  const { user } = useUserDetails();
+  const userId = user?.uid;
+  const userCartRef = ref(db, "users/" + userId + "/cart/cart/");
+  useEffect(() => {
+    // Fetch user cart data when the userCartRef changes
+    const unsubscribe = onValue(userCartRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const newCart = snapshot.val();
 
-				// Check if the cart data has changed before updating the state
-				if (JSON.stringify(newCart) !== JSON.stringify(cart)) {
-					setCart(newCart);
-				}
-			} else {
-				console.log("No data available");
-			}
-		});
+        // Check if the cart data has changed before updating the state
+        if (JSON.stringify(newCart) !== JSON.stringify(cart)) {
+          setCart(newCart);
+        }
+      } else {
+        console.log("No data available");
+      }
+    });
 
-		return () => {
-			// Unsubscribe when the component unmounts
-			unsubscribe();
-		};
-	}, [userCartRef, setCart, cart]);
+    return () => {
+      // Unsubscribe when the component unmounts
+      unsubscribe();
+    };
+  }, [userCartRef, setCart, cart]);
 
-	const addToProfile = (data) => {
-		const userId = user?.uid;
-		const setProfileRef = ref(db, "users/" + userId + "/orders/");
+  const addToProfile = (data) => {
+    const userId = user?.uid;
+    const setProfileRef = ref(db, "users/" + userId + "/orders/");
 
-		// Use the get function to retrieve the data
-		get(setProfileRef)
-			.then((snapshot) => {
-				if (snapshot.exists()) {
-					// User already has orders, update the existing orders
-					const currentOrders = snapshot.val();
-					const newOrderItem = {
-						orderId: data?.orderId,
-						trackingId: data?.trackingId,
-						orderStatus: data?.orderStatus,
-						amount: data?.amount,
-						items: cart,
-					};
+    // Use the get function to retrieve the data
+    get(setProfileRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          // User already has orders, update the existing orders
+          const currentOrders = snapshot.val();
+          const newOrderItem = {
+            orderId: data?.orderId,
+            trackingId: data?.trackingId,
+            orderStatus: data?.orderStatus,
+            amount: data?.amount,
+            items: cart,
+          };
 
-					set(setProfileRef, {
-						// Append the new order to the existing orders list
-						orders: [...currentOrders.orders, newOrderItem],
-					});
-				} else {
-					// No existing orders, create a new list and add the order
-					const newOrderItem = {
-						orderId: data?.orderId,
-						trackingId: data?.trackingId,
-						orderStatus: data?.orderStatus,
-						amount: data?.amount,
-						items: cart,
-					};
+          set(setProfileRef, {
+            // Append the new order to the existing orders list
+            orders: [...currentOrders.orders, newOrderItem],
+          });
+        } else {
+          // No existing orders, create a new list and add the order
+          const newOrderItem = {
+            orderId: data?.orderId,
+            trackingId: data?.trackingId,
+            orderStatus: data?.orderStatus,
+            amount: data?.amount,
+            items: cart,
+          };
 
-					set(setProfileRef, {
-						orders: [newOrderItem], // Create a new list with the order
-					});
-				}
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+          set(setProfileRef, {
+            orders: [newOrderItem], // Create a new list with the order
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			console.log("started");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("started");
 
-			const response = await axios.post(
-				"https://ccavenuesoff.vercel.app/ccavRequestHandler",
-				`merchant_id=2156706&order_id=${orderId}&currency=INR&amount=${totalAmount}&redirect_url=https%3A%2F%2Fccavenuesoff.vercel.app%2FccavResponseHandler&cancel_url=https%3A%2F%2Fccavenuesoff.vercel.app%2FccavResponseHandler&language=EN`
-			);
-			console.log("response from nodejs: ", response.data);
-			const newW = window.open("", "_blank");
-			newW.document.write(response.data);
-			const orderRef = ref(db, "orders/" + orderId);
-			toast.success("started");
-			onValue(orderRef, (snapshot) => {
-				if (snapshot.exists()) {
-					// The record exists at the specified path
-					const data = snapshot.val();
-					// Do something with the data
-					console.log("Record exists:", data);
-					if (data?.orderStatus == "Success") {
-						toast.success(`orderStatus: ${data?.orderStatus}`);
-						addToProfile(data);
-						set(userCartRef, []);
-						setCart([]);
-					} else if (
-						data?.orderStatus == "Aborted" ||
-						data?.orderStatus == "Failure"
-					) {
-						toast.error("Order Failed!");
-					}
-				} else {
-					// The record does not exist at the specified path
-					console.log("Record does not exist");
-					toast.error("record not found");
-				}
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	};
+      const response = await axios.post(
+        "https://ccavenuesoff.vercel.app/ccavRequestHandler",
+        `merchant_id=2156706&order_id=${orderId}&currency=INR&amount=${totalAmount}&redirect_url=https%3A%2F%2Fccavenuesoff.vercel.app%2FccavResponseHandler&cancel_url=https%3A%2F%2Fccavenuesoff.vercel.app%2FccavResponseHandler&language=EN`
+      );
+      console.log("response from nodejs: ", response.data);
+      const newW = window.open("", "_blank");
+      newW.document.write(response.data);
+      const orderRef = ref(db, "orders/" + orderId);
+      toast.success("started");
+      onValue(orderRef, (snapshot) => {
+        if (snapshot.exists()) {
+          // The record exists at the specified path
+          const data = snapshot.val();
+          // Do something with the data
+          console.log("Record exists:", data);
+          if (data?.orderStatus == "Success") {
+            toast.success(`orderStatus: ${data?.orderStatus}`);
+            addToProfile(data);
+            set(userCartRef, []);
+            setCart([]);
+          } else if (
+            data?.orderStatus == "Aborted" ||
+            data?.orderStatus == "Failure"
+          ) {
+            toast.error("Order Failed!");
+          }
+        } else {
+          // The record does not exist at the specified path
+          console.log("Record does not exist");
+          toast.error("record not found");
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	useEffect(() => {
-		// Calculate totalFee whenever cart changes
-		const newTotalFee = cart.reduce(
-			(total, item) => total + parseInt(item.registrationFee, 10),
-			0
-		);
+  useEffect(() => {
+    // Calculate totalFee whenever cart changes
+    const newTotalFee = cart.reduce(
+      (total, item) => total + parseInt(item.registrationFee, 10),
+      0
+    );
 
-		// Update totalAmount only if it has changed
-		if (newTotalFee !== totalAmount) {
-			setTotalAmount(newTotalFee);
-		}
-	}, [cart, totalAmount]);
+    // Update totalAmount only if it has changed
+    if (newTotalFee !== totalAmount) {
+      setTotalAmount(newTotalFee);
+    }
+  }, [cart, totalAmount]);
 
-	function handleRemoveAll() {
-		const userCartRef = ref(db, "users/" + userId + "/cart/");
-		set(userCartRef, []).then(() => [setCart([])]);
-	}
+  function handleRemoveAll() {
+    const userCartRef = ref(db, "users/" + userId + "/cart/");
+    set(userCartRef, []).then(() => [setCart([])]);
+  }
 
-	return (
-		<>
-			{user ? (
-				<div className=" max-w-7xl pt-[100px] mx-auto">
-					<div className="flex flex-col md:flex-row items-center m-3 mb-6 w-full justify-around">
-						<h1 className="text-2xl font-semibold p-2 text-center">
-							Welcome to cart, {user?.displayName}
-						</h1>
-						<div className="flex flex-col md:flex-row items-center gap-4 p-2">
-							<div className="flex flex-col md:flex-row items-center gap-4">
-								<h1 className="text-lg">Total Amount: ₹ {totalAmount}</h1>
-								<form onSubmit={handleSubmit}>
-									<button
-										type="submit"
-										className="border-2 flex items-center gap-3 border-white rounded-md px-20 md:px-5 py-2 hover:bg-white hover:text-black duration-200 ease-in-out"
-										disabled={cart.length === 0 ? true : false}
-									>
-										<IoBagCheckOutline />
-										Checkout
-									</button>
-								</form>
-							</div>
-							<button
-								onClick={handleRemoveAll}
-								className="border-2 flex text-nowrap items-center gap-3 border-white rounded-md px-20 md:px-5 py-2 hover:bg-white hover:text-black duration-200 ease-in-out"
-							>
-								Remove All
-							</button>
-						</div>
-					</div>
-					{cart.length === 0 && (
-						<div className="flex items-center justify-center h-[70vh]">
-							<h1 className="text-3xl font-semibold">No items in the cart</h1>
-						</div>
-					)}
-					{cart.length > 0 && (
-						<div className=" flex items-center flex-wrap gap-6 justify-center pb-4 ">
-							{cart?.map((item, index) => (
-								<CheckoutCard event={item} key={index} />
-								// You can add additional <p> elements for other properties
-							))}
-						</div>
-					)}
-				</div>
-			) : (
-				<div className="flex items-center justify-center h-screen">
-					<h1 className="text-2xl font-semibold">
-						Please login to view your cart
-					</h1>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {user ? (
+        <div className=" max-w-7xl pt-[100px] mx-auto">
+          <div className="flex z-50 md:relative flex-col md:flex-row items-center m-3 mb-6 w-full justify-around">
+            <h1 className="text-2xl font-semibold p-2 text-center">
+              Welcome to cart, {user?.displayName}
+            </h1>
+            <div className="flex flex-col md:flex-row items-center gap-4 p-2">
+              <div className="flex flex-col md:flex-row items-center gap-4">
+                <h1 className="text-lg">Total Amount: ₹ {totalAmount}</h1>
+                <form onSubmit={handleSubmit}>
+                  <button
+                    type="submit"
+                    className="border-2 flex items-center gap-3 border-white rounded-md px-20 md:px-5 py-2 hover:bg-white hover:text-black duration-200 ease-in-out"
+                    disabled={cart.length === 0 ? true : false}
+                  >
+                    <IoBagCheckOutline />
+                    Checkout
+                  </button>
+                </form>
+              </div>
+              <button
+                onClick={handleRemoveAll}
+                className="border-2 flex text-nowrap items-center gap-3 border-white rounded-md px-20 md:px-5 py-2 hover:bg-white hover:text-black duration-200 ease-in-out"
+              >
+                Remove All
+              </button>
+            </div>
+          </div>
+          {cart.length === 0 && (
+            <div className="flex items-center justify-center h-[70vh]">
+              <h1 className="text-3xl font-semibold">No items in the cart</h1>
+            </div>
+          )}
+          {cart.length > 0 && (
+            <div className=" flex items-center flex-wrap gap-6 justify-center pb-4 ">
+              {cart?.map((item, index) => (
+                <CheckoutCard event={item} key={index} />
+                // You can add additional <p> elements for other properties
+              ))}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-2xl font-semibold">
+            Please login to view your cart
+          </h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Page;
--- app/profile/page.js
+++ app/profile/page.js
@@ -11,156 +11,156 @@ import MyDocument from "@/components/PdfGen";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 
 export default function Page() {
-	const { user } = useUserDetails();
-	const [userDetails, setUserDetails] = useState(null);
-	const [orders, setOrders] = useState([]);
+  const { user } = useUserDetails();
+  const [userDetails, setUserDetails] = useState(null);
+  const [orders, setOrders] = useState([]);
 
-	function createUserDetails() {
-		console.log(userDetails);
-		set(ref(db, "users/" + user?.uid + "/Details"), {
-			userDetails,
-		});
-		toast.success("Profile Updated");
-	}
+  function createUserDetails() {
+    console.log(userDetails);
+    set(ref(db, "users/" + user?.uid + "/Details"), {
+      userDetails,
+    });
+    toast.success("Profile Updated");
+  }
 
-	useEffect(() => {
-		if (user) {
-			const userRef = ref(db, "users/" + user.uid + "/Details/userDetails");
-			const fetchData = (snapshot) => {
-				const data = snapshot.val();
-				if (snapshot.exists() && !userDetails?.clg && !userDetails?.phone) {
-					setUserDetails(data);
-				}
-			};
+  useEffect(() => {
+    if (user) {
+      const userRef = ref(db, "users/" + user.uid + "/Details/userDetails");
+      const fetchData = (snapshot) => {
+        const data = snapshot.val();
+        if (snapshot.exists() && !userDetails?.clg && !userDetails?.phone) {
+          setUserDetails(data);
+        }
+      };
 
-			const errorCallback = (error) => {
-				console.error("Error fetching data:", error);
-			};
+      const errorCallback = (error) => {
+        console.error("Error fetching data:", error);
+      };
 
-			// Set up the Firebase subscription
-			const unsubscribe = onValue(userRef, fetchData, errorCallback);
+      // Set up the Firebase subscription
+      const unsubscribe = onValue(userRef, fetchData, errorCallback);
 
-			// Clean up the subscription when the component unmounts
-			return () => {
-				unsubscribe();
-			};
-		}
-	}, [user?.uid, userDetails]);
+      // Clean up the subscription when the component unmounts
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [user?.uid, userDetails]);
 
-	// for orders
-	useEffect(() => {
-		if (user) {
-			const userOrderRef = ref(db, "users/" + user.uid + "/orders/");
+  // for orders
+  useEffect(() => {
+    if (user) {
+      const userOrderRef = ref(db, "users/" + user.uid + "/orders/");
 
-			const fetchOrders = (snapshot) => {
-				const data = snapshot.val();
-				if (data) {
-					setOrders(Object.values(data));
-				}
-			};
+      const fetchOrders = (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          setOrders(Object.values(data));
+        }
+      };
 
-			const errorCallback = (error) => {
-				console.error("Error fetching data:", error);
-			};
+      const errorCallback = (error) => {
+        console.error("Error fetching data:", error);
+      };
 
-			// Set up the Firebase subscription
-			const unsubscribeOrders = onValue(
-				userOrderRef,
-				fetchOrders,
-				errorCallback
-			);
+      // Set up the Firebase subscription
+      const unsubscribeOrders = onValue(
+        userOrderRef,
+        fetchOrders,
+        errorCallback
+      );
 
-			// Clean up the subscription when the component unmounts
-			return () => {
-				unsubscribeOrders();
-			};
-		}
-	}, [user?.uid]);
-	console.log("orders: ", orders);
-	return (
-		<>
-			{user ? (
-				<div className="pt-[130px]">
-					<div>
-						<div className="flex justify-around p-2 items-center flex-col sm:flex-row">
-							<div className="flex flex-col items-center justify-center gap-7 py-4">
-								<Image
-									src={user?.photoURL}
-									width={200}
-									height={200}
-									alt="profile"
-									className="rounded-md border-2 border-white p-1 w-full"
-								/>
-								<button className=" border-2 border-white rounded-md px-10 py-1 hover:bg-white hover:text-black duration-200 ease-in-out">
-									<PDFDownloadLink
-										document={
-											<MyDocument
-												orders={orders[0]}
-												userD={userDetails}
-												user={user}
-											/>
-										}
-										fileName="Acknowledgement.pdf"
-										className="flex items-center gap-4 text-lg"
-									>
-										<HiArrowDownTray />
-										Download Receipt
-									</PDFDownloadLink>
-								</button>
-							</div>
-							<div className="flex flex-col items-start justify-center gap-5 max-w-4xl">
-								<div className="flex gap-4 text-xl">
-									<p className="">Name:</p>
-									<p>{user?.displayName}</p>
-								</div>
-								<div className="flex gap-4 text-lg w-full">
-									<p className="w-full">College Name:</p>
-									<input
-										type="text"
-										value={userDetails?.clg}
-										className="outline-none items-end rounded-md bg-transparent border border-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white px-2 py-1 text-sm"
-										onChange={(e) =>
-											setUserDetails({ ...userDetails, clg: e.target.value })
-										}
-									/>
-								</div>
-								<div className="flex gap-4 text-lg w-full">
-									<p className="w-full">Phone Number:</p>
-									<input
-										type="number"
-										value={userDetails?.phone}
-										className="outline-none rounded-md bg-transparent border border-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white px-2 py-1 text-sm"
-										onChange={(e) =>
-											setUserDetails({ ...userDetails, phone: e.target.value })
-										}
-									/>
-								</div>
-								<button
-									className="w-full text-lg mt-3 border-2  border-white rounded-md px-3 py-1 hover:bg-white hover:text-black duration-200 ease-in-out text-center"
-									onClick={createUserDetails}
-								>
-									Save
-								</button>
-							</div>
-						</div>
-						<div className="flex items-center justify-start my-8"></div>
-						<div>
-							<h1 className="text-3xl text-center font-bold my-14">ORDERS</h1>
-							<div className="flex flex-wrap justify-center gap-6 pb-4">
-								{orders[0]?.map((cart, cartIndex) => {
-									return cart?.items?.map((item, index) => {
-										return <ProfileOrderCard order={item} key={index} />;
-									});
-								})}
-							</div>
-						</div>
-					</div>
-				</div>
-			) : (
-				<div className="h-screen flex items-center justify-center font-semibold text-2xl">
-					<h1>Please Login to view your profile</h1>
-				</div>
-			)}
-		</>
-	);
+      // Clean up the subscription when the component unmounts
+      return () => {
+        unsubscribeOrders();
+      };
+    }
+  }, [user?.uid]);
+  console.log("orders: ", orders);
+  return (
+    <>
+      {user ? (
+        <div className="pt-[130px]">
+          <div>
+            <div className="flex z-50 md:relative justify-around p-2 items-center flex-col sm:flex-row">
+              <div className="flex flex-col items-center justify-center gap-7 py-4">
+                <Image
+                  src={user?.photoURL}
+                  width={200}
+                  height={200}
+                  alt="profile"
+                  className="rounded-md border-2 border-white p-1 w-full"
+                />
+                <button className=" border-2 border-white rounded-md px-10 py-1 hover:bg-white hover:text-black duration-200 ease-in-out">
+                  <PDFDownloadLink
+                    document={
+                      <MyDocument
+                        orders={orders[0]}
+                        userD={userDetails}
+                        user={user}
+                      />
+                    }
+                    fileName="Acknowledgement.pdf"
+                    className="flex items-center gap-4 text-lg"
+                  >
+                    <HiArrowDownTray />
+                    Download Receipt
+                  </PDFDownloadLink>
+                </button>
+              </div>
+              <div className="flex flex-col items-start justify-center gap-5 max-w-4xl">
+                <div className="flex gap-4 text-xl">
+                  <p className="">Name:</p>
+                  <p>{user?.displayName}</p>
+                </div>
+                <div className="flex gap-4 text-lg w-full">
+                  <p className="w-full">College Name:</p>
+                  <input
+                    type="text"
+                    value={userDetails?.clg}
+                    className="outline-none items-end rounded-md bg-transparent border border-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white px-2 py-1 text-sm"
+                    onChange={(e) =>
+                      setUserDetails({ ...userDetails, clg: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="flex gap-4 text-lg w-full">
+                  <p className="w-full">Phone Number:</p>
+                  <input
+                    type="number"
+                    value={userDetails?.phone}
+                    className="outline-none rounded-md bg-transparent border border-white hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white px-2 py-1 text-sm"
+                    onChange={(e) =>
+                      setUserDetails({ ...userDetails, phone: e.target.value })
+                    }
+                  />
+                </div>
+                <button
+                  className="w-full text-lg mt-3 border-2  border-white rounded-md px-3 py-1 hover:bg-white hover:text-black duration-200 ease-in-out text-center"
+                  onClick={createUserDetails}
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-start my-8"></div>
+            <div>
+              <h1 className="text-3xl text-center font-bold my-14">ORDERS</h1>
+              <div className="flex flex-wrap justify-center gap-6 pb-4">
+                {orders[0]?.map((cart, cartIndex) => {
+                  return cart?.items?.map((item, index) => {
+                    return <ProfileOrderCard order={item} key={index} />;
+                  });
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="h-screen flex items-center justify-center font-semibold text-2xl">
+          <h1>Please Login to view your profile</h1>
+        </div>
+      )}
+    </>
+  );
 }
