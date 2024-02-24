cddfc17d629ca940895717478c29bd5920ee0189
thekavikumar <kavikumarceo@gmail.com>
2024-02-24 09:17:26 +0530

    made s

--- app/travel/page.js
+++ app/travel/page.js
@@ -5,49 +5,52 @@ function Page() {
     <div className=" max-w-7xl z-10 relative pt-[100px] mx-auto">
       <div className="flex flex-col justify-center text-justify p-6">
         <h1 className="text-4xl font-semibold text-center">Travel</h1>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">Route 1: </span>egmore → nehru
-          park → kilpauk → pachaiappas → dpi → ampa skywalk → shanti colony →
-          roundana → k4 → icf south colony → icf rail museum → nathamuni → sidco
-          → padi → drj → senthil → temple school → retteri → madhavaram → puzhal
-          → redhills → alamathi → edapakkam → tamaraipakkam → vengal
+        <p className="text-lg max-w-3xl mx-auto text-zinc-400  font-normal uppercase text-justify mt-4">
+          <span className="font-bold text-xl text-white">Route 1: </span>egmore
+          → nehru park → kilpauk → pachaiappas → dpi → ampa skywalk → shanti
+          colony → roundana → k4 → icf south colony → icf rail museum →
+          nathamuni → sidco → padi → drj → senthil → temple school → retteri →
+          madhavaram → puzhal → redhills → alamathi → edapakkam → tamaraipakkam
+          → vengal
         </p>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">Route 2: </span>kilambakam →
-          vandaloor → mudichoor → varadarajapiuram toll gate → tirumudivakkam →
-          kundrathur → mata engineering college → nazarethpet → nemilicheri →
-          tiruninravur → pakkam → tamaraipakkam → vengal
+        <p className="text-lg max-w-3xl mx-auto uppercase text-zinc-400 font-normal text-justify mt-4">
+          <span className="font-bold text-xl text-white">Route 2: </span>
+          kilambakam → vandaloor → mudichoor → varadarajapiuram toll gate →
+          tirumudivakkam → kundrathur → mata engineering college → nazarethpet →
+          nemilicheri → tiruninravur → pakkam → tamaraipakkam → vengal
         </p>
       </div>
       <div className="flex flex-col justify-center text-justify p-6">
         <h1 className="text-4xl font-semibold text-center">Accommodation</h1>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">First Come, First Serve: </span>
+        <p className="text-lg max-w-3xl mx-auto font-normal text-zinc-400 text-justify mt-4">
+          <span className="font-bold text-xl text-white">
+            First Come, First Serve:{" "}
+          </span>
           Our accommodation is provided on a first-come, first-serve basis. We
           recommend booking early to secure your spot and avoid any
           inconvenience.
         </p>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">Facilities: </span>
+        <p className="text-lg max-w-3xl mx-auto font-normal text-zinc-400 text-justify mt-4">
+          <span className="font-bold text-xl text-white">Facilities: </span>
           Our accommodation facilities include clean and spacious bathrooms, and
           access to Food lounge Food: We provide delicious and nutritious meals
           for our guests.{" "}
         </p>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
+        <p className="text-lg max-w-3xl mx-auto font-normal text-zinc-400 text-justify mt-4">
           Our menu includes a variety of options to cater to different dietary
           preferences and requirements. We also offer special meals for guests
           with specific dietary needs, so please let us know in advance if you
           have any special requests.{" "}
         </p>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">Security: </span>
+        <p className="text-lg max-w-3xl mx-auto font-normal text-zinc-400 text-justify mt-4">
+          <span className="font-bold text-xl text-white">Security: </span>
           Your safety and security are our top priorities. Our accommodation is
           monitored by security personnel, and we have strict access control
           measures in place to ensure a safe and comfortable environment for all
           our guests.{" "}
         </p>
-        <p className="text-lg max-w-3xl mx-auto font-normal text-justify mt-4">
-          <span className="font-bold text-xl">Booking: </span>
+        <p className="text-lg max-w-3xl mx-auto font-normal text-zinc-400 text-justify mt-4">
+          <span className="font-bold text-xl text-white">Booking: </span>
           To book your accommodation, please contact us directly(phone number:
           +917010962506). We look forward to welcoming you and providing you
           with a memorable experience! Please feel free to reach out to us if
--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -4,28 +4,29 @@ import { FaFacebookF } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 
 const SocialMedia = () => {
-	return (
-		<div className="absolute flex lg:flex-col bottom-8 left-[50%] -translate-x-[50%] lg:bottom-[50%] lg:translate-y-[50%] lg:left-10 gap-12 text-white text-xl font-medium">
-			<a
-				href=""
-				className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
-			>
-				<FaInstagram />
-			</a>
-			<a
-				href=""
-				className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
-			>
-				<FaFacebookF />
-			</a>
-			<a
-				href=""
-				className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
-			>
-				<FaXTwitter />
-			</a>
-		</div>
-	);
+  return (
+    <div className="absolute flex lg:flex-col bottom-8 left-[50%] -translate-x-[50%] lg:bottom-[50%] lg:translate-y-[50%] lg:left-10 gap-12 text-white text-xl font-medium">
+      <a
+        href="http://www.instagram.com/tantrotsav_avv/"
+        target="_blank"
+        className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
+      >
+        <FaInstagram />
+      </a>
+      <a
+        href=""
+        className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
+      >
+        <FaFacebookF />
+      </a>
+      <a
+        href=""
+        className="rounded-full p-3 duration-200 ease-in-out border-2 border-white hover:bg-white hover:text-black"
+      >
+        <FaXTwitter />
+      </a>
+    </div>
+  );
 };
 
 export default SocialMedia;
