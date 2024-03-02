d2ead295f6518e64619888404df253eca464057f
thekavikumar <kavikumarceo@gmail.com>
2024-03-02 12:21:28 +0530

    master

--- components/Sponsor.js
+++ components/Sponsor.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import zarobay from "./zarobay.svg";
 import sponlogo from "./sponlogo.jpeg";
+import MISTY from "./MISTY.png";
 
 function Sponsor() {
   return (
@@ -16,6 +17,9 @@ function Sponsor() {
         </div>
         <div className="flex flex-col items-center gap-3">
           <Image src={sponlogo} alt="sponsor" width={140} height={140} />
+        </div>
+        <div className="flex flex-col items-center gap-3">
+          <Image src={MISTY} alt="sponsor" width={140} height={140} />
         </div>
       </div>
     </div>
